98ff3a4fc40ec1420814c45f5080aa853eebfa68
volux <andrey.skulov@gmail.com>
2015-11-10 00:33:17 +0300

    Fix config

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ module.exports = function (options) {
 
   for (var key in options.defaults) if (options.defaults.hasOwnProperty(key)) {
 
-    process.env[key] = process.env[key] || options[key];
+    process.env[key] = process.env[key] || options.defaults[key];
   }
 
   process.env['MONGO_URL'] = process.env['MONGOLAB_URI'] || process.env['MONGO_URL'];
